6386c10db36d2d223889c0c391ffb8661ef9be76
NamraAfzal <namra.afzal@xprolabs.com>
2024-10-21 12:30:05 +0500

    school project

---
+++ db/migrate/20241021072425_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :students do |t|
+      t.string :email
+      t.integer :age
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
